e552298fba78c870487f1141cbeec61eebb342b5
Ericles Piana <pianaericles@gmail.com>
2024-06-27 16:36:26 -0300

    router task done

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import homeLogo from '../../assets/home.png'
 
 function Home() {
     return (
@@ -14,11 +13,6 @@ function Home() {
               
                 <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
               </div>
-            </div>
-  
-            <div className="flex justify-center ">
-              <img src={homeLogo} alt="" className='w-2/3' />
-      
             </div>
           </div>
         </div>
