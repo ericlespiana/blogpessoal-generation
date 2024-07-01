85a356577283330e7e47d503e4709da5072930fa
Ericles Piana <pianaericles@gmail.com>
2024-06-30 22:20:30 -0300

    recurso tema done

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,31 @@ import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
     <>
-        <AuthProvider >
-          <BrowserRouter>
-            <Navbar />
-            <div className='min-h-[80vh]'>
-              <Routes>
-                <Route path="/" element={<Cadastro />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/cadastro" element={<Cadastro />} />
-                <Route path="/home" element={<Home />} />
-              </Routes>
-            </div>
-            <Footer />
-          </BrowserRouter>
-        </AuthProvider>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </AuthProvider>
     </>
   );
 }
